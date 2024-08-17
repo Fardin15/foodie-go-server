52addc31f234f77c13b6006a200060a58624490a
Fardin15 <mohammedshowrav15@gmail.com>
2024-08-17 11:39:58 +0600

    databse and collection created

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ app.use(
 );
 app.use(express.json());
 
-// 2X5FJMAdn2X3@zi
-// foodie-go-database
+console.log(process.env.MONGO_USERNAME);
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://foodie-go-database:2X5FJMAdn2X3@zi@cluster0.lnrcai2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lnrcai2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,6 +30,69 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    // database collection
+    const productsCollection = client.db("mhStoreDb").collection("products");
+    const categoryCollection = client.db("mhStoreDb").collection("category");
+
+    // get api for category
+    // app.get("/category", async (req, res) => {
+    //   const result = await categoryCollection.find().toArray();
+    //   res.send(result);
+    // });
+
+    // get api for products
+    // app.get("/products", async (req, res) => {
+    //   const {
+    //     page = 1,
+    //     limit = 8,
+    //     sortBy = "price",
+    //     order = "asc",
+    //     search = "",
+    //     category,
+    //     brand,
+    //     minPrice,
+    //     maxPrice,
+    //   } = req.query;
+
+    //   // pagination
+    //   const skip = (page - 1) * limit;
+    //   const sortOrder = order === "asc" ? 1 : -1;
+
+    //   const filter = {};
+
+    //   // Handle category filtering
+    //   if (category && category !== "all") {
+    //     filter.category = category;
+    //   }
+    //   // Handle brand filtering
+    //   if (brand) filter.brand = brand;
+    //   // Handle search filtering
+    //   if (search) filter.productName = { $regex: search, $options: "i" };
+
+    //   // Handle price range filtering
+    //   if (minPrice || maxPrice) {
+    //     filter.price = {};
+    //     if (minPrice) filter.price.$gte = parseFloat(minPrice);
+    //     if (maxPrice) filter.price.$lte = parseFloat(maxPrice);
+    //   }
+
+    //   const products = await productsCollection
+    //     .find(filter)
+    //     .sort({ [sortBy]: sortOrder })
+    //     .skip(skip)
+    //     .limit(parseInt(limit))
+    //     .toArray();
+
+    //   const total = await productsCollection.countDocuments(filter);
+
+    //   res.json({
+    //     totalPages: Math.ceil(total / limit),
+    //     currentPage: parseInt(page),
+    //     products,
+    //   });
+    // });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
